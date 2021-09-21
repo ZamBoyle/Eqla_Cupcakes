dec2765a7269951343db4ca74f55cab9f80aa692
ZamBoyle <johnny.piette@gmail.com>
2021-09-21 10:54:58 +0200

    Initial Commit

---
+++ Laravel/resources/views/search.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot name="title">Recherches</x-slot>
+
+    <div class="container my-1 rounded text-justify">
+        <h1 class="text-center">Recherche de Produits</h1>
+    </div>
+</x-layout>
